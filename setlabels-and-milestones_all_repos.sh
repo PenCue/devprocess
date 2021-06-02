9b3df1e2688168516a71d9820759131745076311
Mike Wessling <mwessling@gmail.com>
2021-06-02 10:17:07 +0200

    added all_repos script

---
+++ setlabels-and-milestones_all_repos.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+./setlabels-and-milestones.sh PenCue/timber_azure
+./setlabels-and-milestones.sh PenCue/timber_core
+./setlabels-and-milestones.sh PenCue/timber_test
+./setlabels-and-milestones.sh PenCue/timber_schema
+./setlabels-and-milestones.sh PenCue/timber_docs
+./setlabels-and-milestones.sh PenCue/bucksaw
+./setlabels-and-milestones.sh PenCue/chainsaw
